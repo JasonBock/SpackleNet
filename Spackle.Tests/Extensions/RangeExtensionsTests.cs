b5b3871eb0c16cb023b3555a4e9f138d27a694a9
Jason Bock <jason@jasonbock.net>
2020-05-03 21:48:54 -0500

    #21 added extension methods for range

--- Spackle.Tests/Extensions/RangeExtensionsTests.cs
+++ Spackle.Tests/Extensions/RangeExtensionsTests.cs
@@ -1,18 +1,143 @@
 ﻿using Spackle.Extensions;
+using System;
 using Xunit;
 
 namespace Spackle.Tests.Extensions
 {
 	public static class RangeExtensionsTests
 	{
+		[Theory]
+		[InlineData(0, 5, 3, true)]
+		[InlineData(0, 5, 7, false)]
+		[InlineData(5, 0, 3, true)]
+		[InlineData(5, 0, 7, false)]
+		public static void Contains(int start, int end, int value, bool expectedValue) => 
+			Assert.Equal(expectedValue, (start..end).Contains(value));
+
 		[Fact]
-		public static void Partition()
+		public static void Intersection()
+		{
+			var range = 3..6;
+			var intersection = range.Intersect(5..8)!;
+
+			Assert.Equal(5, intersection.Value.Start);
+			Assert.Equal(6, intersection.Value.End);
+		}
+
+		[Fact]
+		public static void IntersectionWithRangesReversed()
+		{
+			var range = 5..8;
+			var intersection = range.Intersect(3..6)!;
+
+			Assert.Equal(5, intersection.Value.Start);
+			Assert.Equal(6, intersection.Value.End);
+		}
+
+		[Fact]
+		public static void IntersectionWithNoIntersection() => 
+			Assert.Null((3..6).Intersect(7..8));
+
+		[Fact]
+		public static void IntersectionWithEndAndStartEqual()
+		{
+			var range = 3..6;
+			var intersection = range.Intersect(6..8)!;
+
+			Assert.Equal(6, intersection.Value.Start);
+			Assert.Equal(6, intersection.Value.End);
+		}
+
+		[Fact]
+		public static void PartitionWithAscendingRange()
 		{
 			var range = 5..5000;
 			var partitions = range.Partition(5);
 
 			Assert.Equal(5, partitions.Length);
+
 			Assert.Equal(5, partitions[0].Start);
+			Assert.Equal(1004, partitions[0].End);
+
+			Assert.Equal(1005, partitions[1].Start);
+			Assert.Equal(2003, partitions[1].End);
+
+			Assert.Equal(2004, partitions[2].Start);
+			Assert.Equal(3002, partitions[2].End);
+
+			Assert.Equal(3003, partitions[3].Start);
+			Assert.Equal(4001, partitions[3].End);
+
+			Assert.Equal(4002, partitions[4].Start);
+			Assert.Equal(5000, partitions[4].End);
+		}
+
+		[Fact]
+		public static void PartitionWithDescendingRange()
+		{
+			var range = 5000..5;
+			var partitions = range.Partition(5);
+
+			Assert.Equal(5, partitions.Length);
+
+			Assert.Equal(5000, partitions[0].Start);
+			Assert.Equal(4002, partitions[0].End);
+
+			Assert.Equal(4001, partitions[1].Start);
+			Assert.Equal(3003, partitions[1].End);
+
+			Assert.Equal(3002, partitions[2].Start);
+			Assert.Equal(2004, partitions[2].End);
+
+			Assert.Equal(2003, partitions[3].Start);
+			Assert.Equal(1005, partitions[3].End);
+
+			Assert.Equal(1004, partitions[4].Start);
+			Assert.Equal(5, partitions[4].End);
+		}
+
+		[Fact]
+		public static void PartitionWithInvalidNumberOfRanges() => 
+			Assert.Throws<ArgumentException>(() => (1..2).Partition(0));
+
+		[Fact]
+		public static void PartitionWithEqualStartAndEndRangeValues() =>
+			Assert.Throws<ArgumentException>(() => (1..1).Partition(3));
+
+		[Fact]
+		public static void ToAscendingNoChange()
+		{
+			var range = (3..6).ToAscending();
+
+			Assert.Equal(3, range.Start);
+			Assert.Equal(6, range.End);
+		}
+
+		[Fact]
+		public static void ToAscendingWithChange()
+		{
+			var range = (6..3).ToAscending();
+
+			Assert.Equal(3, range.Start);
+			Assert.Equal(6, range.End);
+		}
+
+		[Fact]
+		public static void ToDescendingNoChange()
+		{
+			var range = (6..3).ToDescending();
+
+			Assert.Equal(6, range.Start);
+			Assert.Equal(3, range.End);
+		}
+
+		[Fact]
+		public static void ToDescendingWithChange()
+		{
+			var range = (3..6).ToDescending();
+
+			Assert.Equal(6, range.Start);
+			Assert.Equal(3, range.End);
 		}
 	}
 }
--- Spackle/Extensions/RangeExtensions.cs
+++ Spackle/Extensions/RangeExtensions.cs
@@ -5,9 +5,55 @@ namespace Spackle.Extensions
 {
 	public static class RangeExtensions
 	{
+		public static bool Contains(this Range @this, Index value) =>
+			@this.Contains(value.Value);
+
+		public static bool Contains(this Range @this, int value) =>
+			@this.Start.Value < @this.End.Value ?
+				(value.CompareTo(@this.Start.Value) >= 0 && value.CompareTo(@this.End.Value) <= 0) :
+				(value.CompareTo(@this.Start.Value) <= 0 && value.CompareTo(@this.End.Value) >= 0);
+
+		public static Range? Intersect(this Range @this, Range target)
+		{
+			var currentRange = @this.ToAscending();
+			var targetRange = target.ToAscending();
+
+			if (currentRange.Contains(target.Start) || currentRange.Contains(target.End))
+			{
+				var intersectionStart = currentRange.Start.Value.CompareTo(targetRange.Start.Value) >= 0 ? currentRange.Start : targetRange.Start;
+				var intersectionEnd = currentRange.End.Value.CompareTo(targetRange.End.Value) <= 0 ? currentRange.End : targetRange.End;
+				return intersectionStart..intersectionEnd;
+			}
+
+			return null;
+		}
+
+		public static Range ToAscending(this Range @this) =>
+			@this.Start.Value < @this.End.Value ? @this : @this.End..@this.Start;
+
+		public static Range ToDescending(this Range @this) =>
+			@this.Start.Value > @this.End.Value ? @this : @this.End..@this.Start;
+
 		// https://softwareengineering.stackexchange.com/questions/187680/algorithm-for-dividing-a-range-into-ranges-and-then-finding-which-range-a-number
 		public static ImmutableArray<Range> Partition(this Range @this, int numberOfRanges)
 		{
+			if(numberOfRanges <= 0)
+			{
+				throw new ArgumentException("The number of ranges must be greater than 0.", nameof(numberOfRanges));
+			}
+
+			if(@this.Start.Value == @this.End.Value)
+			{
+				throw new ArgumentException($"The start and end values, {@this.Start.Value}, are the same.", nameof(@this));
+			}
+
+			var shouldReverse = @this.Start.Value > @this.End.Value;
+
+			if(shouldReverse)
+			{
+				@this = @this.End..@this.Start;
+			}
+
 			var highestLength = (@this.End.Value - @this.Start.Value + 1) / numberOfRanges;
 			var bucketSizes = new int[numberOfRanges];
 			Array.Fill(bucketSizes, highestLength);
@@ -28,8 +74,13 @@ namespace Spackle.Extensions
 
 			for (var i = 0; i < numberOfRanges; i++)
 			{
-				ranges.Add(new Range(k, k + bucketSizes[i] - 1));
+				ranges.Add(shouldReverse ? new Range(k + bucketSizes[i] - 1, k) : new Range(k, k + bucketSizes[i] - 1));
 				k += bucketSizes[i];
+			}
+
+			if(shouldReverse)
+			{
+				ranges.Reverse();
 			}
 
 			return ranges.ToImmutable();
