92f7139948adf67c1dcb28db38af4afd3f12a96f
Jason Bock <jason.r.bock@outlook.com>
2022-12-04 14:13:31 -0600

    #39 added AddPairs()

---
+++ Spackle.Tests/Extensions/IDictionaryOfTExtensionsTests.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+using Spackle.Extensions;
+using System;
+using System.Collections.Generic;
+
+namespace Spackle.Tests.Extensions;
+
+public static class IDictionaryOfTExtensionsTests
+{
+	[Test]
+	public static void AddPairs()
+	{
+		var target = new Dictionary<int, string>
+		{
+			{ 1, "a" },
+			{ 2, "b" },
+		};
+
+		var pairs = new Dictionary<int, string>
+		{
+			{ 3, "c" },
+			{ 4, "d" },
+		};
+
+		target.AddPairs(pairs);
+
+		Assert.Multiple(() =>
+		{
+			Assert.That(target.Count, Is.EqualTo(4));
+			Assert.That(target[3], Is.EqualTo("c"));
+			Assert.That(target[4], Is.EqualTo("d"));
+		});
+	}
+
+	[Test]
+	public static void AddPairsWhenKeyExists()
+	{
+		var target = new Dictionary<int, string>
+		{
+			{ 1, "a" },
+			{ 2, "b" },
+		};
+
+		var pairs = new Dictionary<int, string>
+		{
+			{ 3, "c" },
+			{ 2, "d" },
+		};
+
+		Assert.Throws<ArgumentException>(() => target.AddPairs(pairs));
+
+		Assert.Multiple(() =>
+		{
+			Assert.That(target.Count, Is.EqualTo(3));
+			Assert.That(target[3], Is.EqualTo("c"));
+		});
+	}
+
+	[Test]
+	public static void AddPairsWhenTargetIsNull() =>
+		Assert.Throws<ArgumentNullException>(() => (null as IDictionary<string, int>)!.AddPairs(new Dictionary<string, int>()));
+
+	[Test]
+	public static void AddPairsWhenPairsIsNull() =>
+		Assert.Throws<ArgumentNullException>(() => new Dictionary<string, int>().AddPairs((null as IDictionary<string, int>)!));
+}
---
+++ Spackle/Extensions/IDictionaryOfTExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Spackle.Extensions;
+
+public static class IDictionaryOfTExtensions
+{
+	public static void AddPairs<TKey, TValue>(this IDictionary<TKey, TValue> self, IDictionary<TKey, TValue> pairs)
+	{
+		ArgumentNullException.ThrowIfNull(self);
+		ArgumentNullException.ThrowIfNull(pairs);
+
+		foreach (var (key, value) in pairs)
+		{
+			self.Add(key, value);
+		}
+	}
+}
