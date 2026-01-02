620a4b7c03ddf535f4f7516b6c9775d3df50c658
Jason Bock <jason.r.bock@outlook.com>
2026-01-02 10:28:07 -0600

    #50 did IndecesOf() for char

--- src/Spackle.Tests/Extensions/StringExtensionsTests.cs
+++ src/Spackle.Tests/Extensions/StringExtensionsTests.cs
@@ -18,6 +18,44 @@ internal static class StringExtensionsTests
 		Assert.That(() => (null as string)!.AsUri(), Throws.TypeOf<ArgumentNullException>());
 
 	[Test]
+	public static void GetIndecesWhenOneCharacterExists()
+	{
+		var content = "abcdef";
+		var indeces = content.IndecesOf('d');
+
+		using (Assert.EnterMultipleScope())
+		{
+			Assert.That(indeces, Has.Length.EqualTo(1));
+			Assert.That(indeces[0], Is.EqualTo(3));
+		}
+	}
+
+	[Test]
+	public static void GetIndecesWhenMultipleCharactersExists()
+	{
+		var content = "adbcddefd";
+		var indeces = content.IndecesOf('d');
+
+		using (Assert.EnterMultipleScope())
+		{
+			Assert.That(indeces, Has.Length.EqualTo(4));
+			Assert.That(indeces[0], Is.EqualTo(1));
+			Assert.That(indeces[1], Is.EqualTo(4));
+			Assert.That(indeces[2], Is.EqualTo(5));
+			Assert.That(indeces[3], Is.EqualTo(8));
+		}
+	}
+
+	[Test]
+	public static void GetIndecesWhenNoCharactersExists()
+	{
+		var content = "abcdef";
+		var indeces = content.IndecesOf('g');
+
+		Assert.That(indeces, Has.Length.EqualTo(0));
+	}
+
+	[Test]
 	public static void TryAsUri()
 	{
 		const string Site = "http://www.goodsite.com";
--- src/Spackle/Extensions/StringExtensions.cs
+++ src/Spackle/Extensions/StringExtensions.cs
@@ -5,20 +5,42 @@
 /// </summary>
 public static class StringExtensions
 {
-	/// <summary>
-	/// Changes a <see cref="string"/> into a <see cref="Uri"/>
-	/// </summary>
-	/// <param name="self">The <see cref="string"/> to transform.</param>
-	/// <returns>A new <see cref="Uri"/>.</returns>
-	/// <exception cref="ArgumentNullException">Thrown if <paramref name="self"/> is <see langword="null" />.</exception>
-	public static Uri AsUri(this string self) => new(self);
+	extension(string self)
+	{
+		/// <summary>
+		/// Changes a <see cref="string"/> into a <see cref="Uri"/>
+		/// </summary>
+		/// <returns>A new <see cref="Uri"/>.</returns>
+		public Uri AsUri() => new(self);
 
-	/// <summary>
-	/// Changes a <see cref="string"/> into a <see cref="Uri"/> if it can.
-	/// </summary>
-	/// <param name="self">The <see cref="string"/> to transform.</param>
-	/// <param name="result">The new <see cref="Uri"/> if the given string could be transforms into a <see cref="Uri"/> .</param>
-	/// <param name="kind">The <see cref="UriKind"/> to use for <paramref name="self"/>.</param>
-	public static bool TryAsUri(this string self, out Uri? result, UriKind kind = UriKind.Absolute) =>
-		Uri.TryCreate(self, kind, out result);
+		/// <summary>
+		/// Gets all the indeces of <paramref name="value"/>.
+		/// </summary>
+		/// <param name="value">The value to look for in <paramref name="self"/>.</param>
+		/// <returns></returns>
+		public int[] IndecesOf(char value)
+		{
+			var indeces = new List<int>();
+
+			var startIndex = 0;
+			var index = self.IndexOf(value, startIndex);
+
+			while (index != -1)
+			{
+				indeces.Add(index);
+				startIndex = index + 1;
+				index = self.IndexOf(value, startIndex);
+			}
+
+			return [.. indeces];
+		}
+
+		/// <summary>
+		/// Changes a <see cref="string"/> into a <see cref="Uri"/> if it can.
+		/// </summary>
+		/// <param name="result">The new <see cref="Uri"/> if the given string could be transforms into a <see cref="Uri"/> .</param>
+		/// <param name="kind">The <see cref="UriKind"/> to use for <c>self</c>.</param>
+		public bool TryAsUri(out Uri? result, UriKind kind = UriKind.Absolute) =>
+			Uri.TryCreate(self, kind, out result);
+	}
 }
